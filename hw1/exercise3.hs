93f4b10af1420a8ddc68bfb7346336a636db7566
Spaceman Bob <85317525+heycryptobob@users.noreply.github.com>
2021-09-07 14:53:41 +0200

    Create exercise3.hs

---
+++ hw1/exercise3.hs
@@ -0,0 +1,44 @@
+{-# OPTIONS_GHC -fno-warn-warnings-deprecations -fno-warn-unused-binds #-}
+import CodeWorld
+
+main :: IO ()
+main = exercise3
+
+wall, ground, storage, box :: Picture
+wall = colored gray (solidRectangle 1 1)
+ground = colored (light gray) (solidRectangle 1 1)
+storage = solidCircle 0.25 & ground
+box = colored brown (solidRectangle 1 1)
+
+drawTile :: Integer -> Picture
+drawTile 1 = wall
+drawTile 2 = ground
+drawTile 3 = storage
+drawTile 4 = box
+drawTile _ = blank
+
+pictureOfMaze :: Picture
+pictureOfMaze = drawRows (-10)
+
+drawRows :: Integer -> Picture
+drawRows 11 = blank
+drawRows r = drawCols r (-10) & drawRows (r+1)
+
+drawCols :: Integer -> Integer -> Picture
+drawCols _ 11 = blank
+drawCols r c = drawTileAt r c & drawCols r (c+1)
+
+drawTileAt :: Integer -> Integer -> Picture
+drawTileAt r c = translated (fromIntegral r) (fromIntegral c) (drawTile (maze r c))
+
+exercise3 = drawingOf pictureOfMaze
+         
+maze :: Integer -> Integer -> Integer 
+maze x y
+  | abs x > 4  || abs y > 4  = 0
+  | abs x == 4 || abs y == 4 = 1
+  | x ==  2 && y <= 0        = 1
+  | x ==  3 && y <= 0        = 3
+  | x >= -2 && y == 0        = 4
+  | otherwise                = 2
+ 
